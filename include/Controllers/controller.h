a23f38cde9b433bfa39f315d192eeacb18113149
Colin - UNI <ccm@uni.edu>
2026-09-24 17:58:38 -0500

    Sep-24-26
    Unbroke stuff

--- include/Controllers/controller.h
+++
@@ -1,9 +0,0 @@
-#include <Arduino.h>
-#include <Bluepad32.h>
-
-
-// Need this for the controllers to work
-ControllerPtr myControllers[BP32_MAX_GAMEPADS];
-
-void onConnectedController(ControllerPtr ctl);
-void onDisconnectedController(ControllerPtr ctl);
--- include/main.h
+++ include/main.h
@@ -1,8 +1,15 @@
 #include <Arduino.h>
 
 #include <Basicmicro.h>
+#include <Bluepad32.h>
 
-#include "Controllers/controller.h"
+
+// Need this for the controllers to work
+ControllerPtr myControllers[BP32_MAX_GAMEPADS];
+
+void onConnectedController(ControllerPtr ctl);
+void onDisconnectedController(ControllerPtr ctl);
+
 
 
 Basicmicro motorDriver(&Serial2, 5000);
