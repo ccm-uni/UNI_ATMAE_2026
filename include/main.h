b6747ca81ab29453c01a524f0e369283db4b5c84
Colin - UNI <ccm@uni.edu>
2026-09-24 18:07:14 -0500

    Sep-24-26.2
    Added more controller support stuff

--- include/main.h
+++ include/main.h
@@ -6,10 +6,46 @@
 
 // Need this for the controllers to work
 ControllerPtr myControllers[BP32_MAX_GAMEPADS];
+Basicmicro motorDriver(&Serial2, 5000);
+
+// Easy way to reach the controller values
+struct ControllerStruct {
+  int16_t
+    LX = 0,
+    LY = 0,
+    RX = 0,
+    RY = 0,
+    L2 = 0,
+    R2 = 0;
+
+  bool
+    dpad_up,
+    dpad_down,
+    dpad_left,
+    dpad_right;
+
+  bool
+    cross = false,
+    circle = false,
+    triangle = false,
+    square = false;
+
+  bool
+    L1 = false,
+    R1 = false;
+
+  bool
+    connected = false;
+
+  byte
+    colorBar[3] = {0, 0, 0};
+
+  byte deadzone = 16;
+};
+ControllerStruct controller;
+
 
 void onConnectedController(ControllerPtr ctl);
 void onDisconnectedController(ControllerPtr ctl);
-
-
-
-Basicmicro motorDriver(&Serial2, 5000);
+void processControllers();
+void processGamepad(ControllerPtr ctl);
--- src/main.cpp
+++ src/main.cpp
@@ -41,9 +41,107 @@ void onDisconnectedController(ControllerPtr ctl) {
   }
 }
 
-void setup() {
+
+/**
+  * Determine which controller proccessor to use
+  * Makes sure the controller is sending proper data
+  */
+void processControllers() {
+
+  // While trying to process the controller, if it doesn't get the proper data, makes this false
+  controller.connected = false;
+
+  for (auto myController : myControllers) {
+
+    // Controller variable error
+    if (!myController) {
+
+    } else if (!myController->isConnected()) {  // No Controller connected
+      Serial.println("ERROR: Controller NOT Connected");
+
+    } else if (!myController->hasData()) {  // No data
+      Serial.println("ERROR: Controller has no Data");
+
+    } else if (!myController->isGamepad()) {  // Not a supported gamepad
+      Serial.println("ERROR: Controller is NOT a Gamepad");
+
+    } else {  // Regular run
+      processGamepad(myController);
+      controller.connected = true;
+    }
+  }
 }
 
+
+/**
+  * Map all BT32 controller values to local struct
+  * 
+  */
+void processGamepad(ControllerPtr ctl) {
+
+  // Connection Good
+  ctl->setColorLED(controller.colorBar[0], controller.colorBar[1], controller.colorBar[2]);
+
+  controller.LX = ctl->axisX();
+  controller.LY = -ctl->axisY();
+  controller.RX = ctl->axisRX();
+  controller.RY = -ctl->axisRY();
+
+  controller.L2 = ctl->l2();
+  controller.R2 = ctl->r2();
+
+  controller.dpad_up = ctl->dpad() & 0x01;
+  controller.dpad_down = ctl->dpad() & 0x02;
+  controller.dpad_right = ctl->dpad() & 0x04;
+  controller.dpad_left = ctl->dpad() & 0x08;
+
+  controller.cross = ctl->a();
+  controller.circle = ctl->b();
+  controller.triangle = ctl->y();
+  controller.square = ctl->x();
+
+  controller.L1 = ctl->l1();
+  controller.R1 = ctl->r1();
+
+
+  // Another way to query controller data is by getting the buttons() function.
+  // See how the different "dump*" functions dump the Controller info.
+  // dumpGamepad(ctl);
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  motorDriver.begin(38400);  // Initialize the motor drivers
+
+  Serial.printf("Firmware: %s\n", BP32.firmwareVersion());
+  const uint8_t* addr = BP32.localBdAddress();
+  Serial.printf("BD Addr: %2X:%2X:%2X:%2X:%2X:%2X\n", addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
+
+  // Setup the Bluepad32 callbacks
+  BP32.setup(&onConnectedController, &onDisconnectedController);
+
+
+  // "forgetBluetoothKeys()" should be called when the user performs
+  // a "device factory reset", or similar.
+  // Calling "forgetBluetoothKeys" in setup() just as an example.
+  // Forgetting Bluetooth keys prevents "paired" gamepads to reconnect.
+  // But it might also fix some connection / re-connection issues.
+  //BP32.forgetBluetoothKeys();
+
+  // Enables mouse / touchpad support for gamepads that support them.
+  // When enabled, controllers like DualSense and DualShock4 generate two connected devices:
+  // - First one: the gamepad
+  // - Second one, which is a "virtual device", is a mouse.
+  // By default, it is disabled.
+  BP32.enableVirtualDevice(false);
+
+}
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Updates the controller once a timer is done.
+  if (BP32.update()) {
+    processControllers();
+  }
 }
